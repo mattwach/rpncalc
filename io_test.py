d9b519541cb74fd3eeb7526e8abe16cc8a65070c
mattwach <matt.wachowski@gmail.com>
2020-09-22 17:34:31 -0700

    Added many tests to io_test.py

--- io_test.py
+++ io_test.py
@@ -36,25 +36,144 @@ DATA = """
   I 0
   O 5.0 x = -5.0 y = 0.0 |>
 
-  I X 5 5 +
+  I D 5 5 +
   O y = 10.0 |>
 
-  I X 5 5 + 2 /
+  I D 5 5 + 2 /
   O y = 5.0 |>
+
+  I D 3 3 * 4 4 * + sqrt
+  O y = 5.0 |>
+
+  I D 3 2 ** 4 2 ** + sqrt
+  O y = 5.0 |>
+
+  I D 3 d * 4 d * + sqrt
+  O y = 5.0 |>
+
+  I D 5 2 ** v:pi *
+  O y = 78.5398163397 |>
+
+  I D -12.34e6
+  O y = -12340000.0 |>
+
+  I D 12.34e-6
+  O y = 1.234e-05 |>
 
 # Trig functions, in radians
 
-  I X 1 sin
+  I D 1 sin
   O y = 0.841470984808 |>
 
 # --- Conversions ---
 
-  I X 10 in>cm
+  I D 10 in>cm
   O y = 25.4 |>
 
-  I X 25.4 cm>in
+  I D 25.4 cm>in
   O y = 10.0 |>
 
+# --- Time Tests ---
+
+  I D 12/25 12/24 -
+  O y = 86400.0 | 1d |time|>
+
+# -- Duration Tests ---
+
+  I normal D 20:00 5000 meters>miles /
+  O y = 386.24256 | 06:26 |dur|>
+
+# --- Money ---
+
+  I normal D $ 1.24 5.34 6.78 sum 1.09 *
+  O y = 14.5624 | $14.56 |$|>
+
+  I D cent -4234 -1215 -500 -2738 50000 sum
+  O y = 41313.0 | $413.13 |cent|>
+
+# --- Hexadecimal ---
+
+  I normal D 0x80
+  O y = 128 | 0x80 |hex|mix|>
+
+  I D -0x1234
+  O y = -4660 | -0x1234 |hex|mix|>
+
+# --- Stack Management ---
+
+  I normal floats D 1 2 3 4 5 6 7 8 9 10 11
+  O ...  2.0  3.0  4.0  5.0  6.0  7.0  8.0  9.0 x = 10.0 y = 11.0 |>
+
+  I .
+  O ...  2.0  3.0  4.0  5.0  6.0  7.0  8.0  9.0 x = 10.0 y = 11.0 |>
+
+  I ..
+  O s11 = 1.0 s10 = 2.0 s9 = 3.0 s8 = 4.0 s7 = 5.0 s6 = 6.0 s5 = 7.0 s4 = 8.0 s3 = 9.0 s2 = 10.0 s1 = 11.0 |>
+
+  I D 1 d
+  O x = 1.0 y = 1.0 |>
+
+  I D 1 2 y
+  O y = 1.0 |>
+
+  I D y
+  E While parsing y: Not Enough Stack Arguments !!
+  O |>
+
+  I D 1 2 3 R
+  O 3.0 x = 2.0 y = 1.0 |>
+
+  I D R
+  O |>
+
+  I D 1 2 3 s
+  O 1.0 x = 3.0 y = 2.0 |>
+
+  I D 1 s
+  E While parsing s: Not Enough Stack Arguments !!
+  O |>
+
+  I D 1 2 3 4 rd
+  O 4.0 1.0 x = 2.0 y = 3.0 |>
+
+  I ru
+  O 1.0 2.0 x = 3.0 y = 4.0 |>
+
+# --- Undo and Redo ---
+
+  I normal D 1 2
+  O x = 1.0 y = 2.0 |>
+
+  I 3 4
+  O 1.0 2.0 x = 3.0 y = 4.0 |>
+
+  I 5 6
+  O 1.0 2.0 3.0 4.0 x = 5.0 y = 6.0 |>
+
+  I X
+  O |>
+
+  I u
+  O 1.0 2.0 3.0 4.0 x = 5.0 y = 6.0 |>
+
+  I u
+  O 1.0 2.0 x = 3.0 y = 4.0 |>
+
+  I u
+  O x = 1.0 y = 2.0 |>
+
+  I r
+  O 1.0 2.0 x = 3.0 y = 4.0 |>
+
+  I r
+  O 1.0 2.0 3.0 4.0 x = 5.0 y = 6.0 |>
+
+  I r
+  O |>
+
+  I r
+  E While parsing r: No Redo History Available !!
+  O |>
 """
 
 class Error(Exception):
@@ -79,7 +198,8 @@ def get_output(fout, max_chars=8192):
     fout.flush()
     line_data.append(fout.read(1))
     max_chars -= 1
-    if line_data[-1] == '>' and line_data[-2] == '|':
+    if ((line_data[-1] == '>' and line_data[-2] == '|') or
+        (line_data[-1] == '!' and line_data[-2] == '!')):
       line = ''.join(line_data)
       data = line.strip().split()
       logging.debug('data -> %s', data)
@@ -90,7 +210,7 @@ def get_output(fout, max_chars=8192):
 def compare_output(line_number, expected, actual):
   if expected != actual:
     raise OutputMismatch('Line %d.  Expected: %s.  Got %s' % (
-          line_number, expected, actual))
+        line_number, expected, actual))
 
 
 def parse_line(line_number, line, p):
@@ -110,14 +230,17 @@ def parse_line(line_number, line, p):
     p.stdin.write('\n')
   elif cmd == 'O':
     compare_output(line_number, tokens[1:], get_output(p.stdout))
+  elif cmd == 'E':
+    compare_output(line_number, tokens[1:], get_output(p.stderr))
   else:
     raise UnknownCommandError('Unknown Command: %s' % line)
 
 def main():
   p = subprocess.Popen(
-    ['python', '-u', 'rpn'],
-    stdin=subprocess.PIPE,
-    stdout=subprocess.PIPE)
+      ['python', '-u', 'rpn'],
+      stdin=subprocess.PIPE,
+      stdout=subprocess.PIPE,
+      stderr=subprocess.PIPE)
 
   get_output(p.stdout)
 
